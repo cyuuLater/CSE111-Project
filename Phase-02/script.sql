9b4d6b70826888e65e8b8cb62cbab15d70592b03
cyuuLater <jolaviano@ucmerced.edu>
2025-11-18 13:00:40 -0800

    Modified existing queries

--- Phase-02/script.sql
+++ Phase-02/script.sql
@@ -1,5 +1,8 @@
--- 1. 
-
+-- 1. Validate login for Brian Lee.
+SELECT u_userkey, u_name
+FROM users
+WHERE u_email = 'brian.lee@ucm.edu' 
+    AND u_password = 'pass123';
 
 
 -- 2. 
@@ -10,8 +13,14 @@
 
 
 
--- 4. 
-
+-- 4. Show Jason Wong's permit, which shows his name, permit number, and vehicle info.
+SELECT u.u_name, v.v_plateno, v.v_maker, v.v_model, 
+       pt.pt_category, p.p_issuedate, p.p_expirationdate, pt.pt_duration
+FROM permit p
+    JOIN permitType pt ON pt.pt_permittypekey = p.p_permittypekey
+    JOIN users u ON p.p_userkey = u.u_userkey
+    JOIN vehicles v ON p.p_vehicleskey = v.v_vehicleskey
+WHERE u.u_name = 'Jason Wong';
 
 
 -- 5. 
@@ -60,8 +69,8 @@ WHERE s.s_status = 0;   -- 0 = available, 1 = occupied
 
 
 -- 15. Checks all the spots that are occupied and the information of who's parked in them
---     (such as their plate num, permit types, permit numbers, spots, zones, and lots)
-SELECT u.u_userkey, u.u_name AS user, v.v_plateno AS plate, pt.pt_category  AS permit_type, 
+--     (such as their plate num, permit types, permit numbers, spots, zones, and lots).
+SELECT u.u_name AS user, v.v_plateno AS plate, pt.pt_category  AS permit_type, 
        p.p_permitnum AS permit_number, s.s_num AS spot, z.z_type AS zone, l.l_name AS lot
 FROM parkingHistory ph
     JOIN vehicles v ON ph.ph_vehicleskey = v.v_vehicleskey
@@ -71,7 +80,7 @@ FROM parkingHistory ph
     JOIN spots s ON ph.ph_spotskey = s.s_spotskey
     JOIN zone z ON z.z_zonekey = s.s_zonekey
     JOIN lot l ON l.l_lotkey = s.s_lotkey
-ORDER BY u.u_userkey;
+ORDER BY SUBSTR(s.s_num, 1, 1);
 
 
 -- 16. Find all spots where Alice Kim can park and their corresponding zone and lot assignments. Note Green
@@ -93,11 +102,12 @@ FROM users u
 WHERE u.u_name = 'Alice Kim'
     AND s.s_status = 0 -- vacant spot
     AND s.s_isactive = 1 -- active spot
-ORDER BY l.l_name, s.s_num;
+ORDER BY SUBSTR(s.s_num, 1, 1), -- A, B, C, D, E
+    CAST(SUBSTR(s.s_num, 2) AS INTEGER); -- 1,2,3...
 
 
 -- 17. Find the users who haven't parked yet and their cooresponding vehicle plate numbers.
-SELECT u.u_userkey, u.u_name, v.v_vehicleskey, v.v_plateno
+SELECT u.u_name, v.v_plateno
 FROM users u
 JOIN vehicles v ON u.u_userkey = v.v_userkey
 LEFT JOIN parkingHistory ph 
