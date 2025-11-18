8bba493285e9376d9f6ae27d39fca2eaf0b0edab
cyuuLater <jolaviano@ucmerced.edu>
2025-11-18 11:17:50 -0800

    Fixed C spots to be in H Zone and added few queries

--- Phase-02/script.sql
+++ Phase-02/script.sql
@@ -61,7 +61,7 @@ WHERE s.s_status = 0;   -- 0 = available, 1 = occupied
 
 -- 15. Checks all the spots that are occupied and the information of who's parked in them
 --     (such as their plate num, permit types, permit numbers, spots, zones, and lots)
-SELECT u.u_name AS user, v.v_plateno AS plate, pt.pt_category  AS permit_type, 
+SELECT u.u_userkey, u.u_name AS user, v.v_plateno AS plate, pt.pt_category  AS permit_type, 
        p.p_permitnum AS permit_number, s.s_num AS spot, z.z_type AS zone, l.l_name AS lot
 FROM parkingHistory ph
     JOIN vehicles v ON ph.ph_vehicleskey = v.v_vehicleskey
@@ -71,30 +71,39 @@ FROM parkingHistory ph
     JOIN spots s ON ph.ph_spotskey = s.s_spotskey
     JOIN zone z ON z.z_zonekey = s.s_zonekey
     JOIN lot l ON l.l_lotkey = s.s_lotkey
-ORDER BY s.s_spotskey;
+ORDER BY u.u_userkey;
 
 
--- 16. 
+-- 16. Find all spots where Alice Kim can park and their corresponding zone and lot assignments. Note Green
+--     Zone (1) allows all user types to park there. Gold Zone (2) allows only Faculty, and H Zone (3) only
+--     allows On-Campus Students.
 SELECT s.s_num AS spot, z.z_type AS zone, l.l_name AS lot
 FROM users u
     JOIN vehicles v ON v.v_userkey = u.u_userkey
     JOIN permit p ON p.p_vehicleskey = v.v_vehicleskey
     JOIN permitType pt ON pt.pt_permittypekey = p.p_permittypekey
-    JOIN spots s ON s.s_zonekey = (
-            CASE 
-                WHEN pt.pt_permittypekey = 6 THEN 1   -- Off-Campus Student → Green Zone
-            END
+    JOIN spots s ON (
+            (pt.pt_category = 'On-Campus Student' AND s.s_zonekey IN (1, 3)) OR
+            (pt.pt_category = 'Off-Campus Student' AND s.s_zonekey = 1) OR
+            (pt.pt_category = 'Guest' AND s.s_zonekey = 1) OR
+            (pt.pt_category = 'Faculty' AND s.s_zonekey IN (1, 2)) 
         )
     JOIN zone z ON z.z_zonekey = s.s_zonekey
     JOIN lot l ON l.l_lotkey = s.s_lotkey
-WHERE u.u_name = 'Daisy Nguyen'
-    AND s.s_status = 0 -- available
-    AND s.s_isactive = 1
-ORDER BY s.s_num;
+WHERE u.u_name = 'Alice Kim'
+    AND s.s_status = 0 -- vacant spot
+    AND s.s_isactive = 1 -- active spot
+ORDER BY l.l_name, s.s_num;
 
 
--- 17. 
-
+-- 17. Find the users who haven't parked yet and their cooresponding vehicle plate numbers.
+SELECT u.u_userkey, u.u_name, v.v_vehicleskey, v.v_plateno
+FROM users u
+JOIN vehicles v ON u.u_userkey = v.v_userkey
+LEFT JOIN parkingHistory ph 
+    ON ph.ph_vehicleskey = v.v_vehicleskey
+WHERE ph.ph_vehicleskey IS NULL
+ORDER BY u.u_userkey;
 
 
 -- 18. 
